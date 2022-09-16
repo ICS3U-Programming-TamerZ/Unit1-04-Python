59423d9a1ab3eff1af3c479aba5314f2c88873de
Tamer Zreg <tamer.zreg@stu.oscb.ca>
2022-09-16 17:28:10 +0000

    Added area_per_rect.py basic_math.py

---
+++ area_per_rect.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+# Created by: Tamer Zreg
+# Created on: Sep. 16, 2022
+# This program calculates and displays the area and perimeter of a 
+# rectangle with length 10cm and width 6cm.
+
+def main():
+    print ("For a rectangle that has dimensions")
+    print ("length 10cm and width 6cm:")
+    print ("")
+    print ("Area = {} cm^2". format (10*6))
+    print ("Perimeter = {} cm". format (2*(10+6)))
+
+
+if __name__ == "__main__":
+  main()
---
+++ basic_math.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+# Created by: Tamer Zreg
+# Created on: Sep. 09, 2022
+# This program does basic math.
+
+def main():
+    print ("29 + 2 = {}". format(29+2))
+    print ("4 - 3 = {}". format(4-3))
+    print ("42 / 2 = {}". format(42/2))
+    print ("6 - 4 / 2 = {}". format(6-4/2))
+    print ("9^3 = {}". format(9**3))
+    print ("1 / 3 = {}". format(1/3))
+    
+if __name__ == "__main__":
+  main()
